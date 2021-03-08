64a678de3b0dedf9f161ce38367641111e88ad74
Madeline Ambrose <ambrose907@gmail.com>
2021-03-07 16:57:47 -0800

    Added USA form validation to index page.

---
+++ AddressSearch/BlazorServer/Models/AddressFormatModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormValidation.Model
+{
+    public class AddressFormModel
+    {
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        public string Province { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+    }
+}
